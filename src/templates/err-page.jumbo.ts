4a6eeca671577cb8cc275ddf25e5be8f407e43bf
kakamband <70383870+kakamband@users.noreply.github.com>
2020-10-14 15:33:24 +0400

    Update err-page.jumbo.ts

--- src/templates/err-page.jumbo.ts
+++ src/templates/err-page.jumbo.ts
@@ -2,5 +2,5 @@ export const errPage = `
 <section id="{{id}}"  class="error-page {{type}}">
   <h1 class="error-page__header">{{error}}</h1>
   <p class="error-page__text">{{text}}</p>
-  <button id="chat-button" type="button" class="error-page__link">Назад к чатам</button>
+  <button id="chat-button" type="button" class="error-page__link">بازگشت به چت ها</button>
 </section>`;
