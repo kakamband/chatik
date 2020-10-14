2c3abc3ed64c8ff75130fa1a6a5e61e597acad9e
kakamband <70383870+kakamband@users.noreply.github.com>
2020-10-14 15:32:38 +0400

    Update chat-users-form.jumbo.ts

--- src/templates/chat-users-form.jumbo.ts
+++ src/templates/chat-users-form.jumbo.ts
@@ -1,8 +1,8 @@
 export const chatUsersForm = `<form class="form form_chat-users" name="chat-users" id="myChatUsersForm" novalidate="">
    <fieldset class="form__fieldset" name="fieldset">
-      <legend class="form__legend">Добавить пользователей</legend>
+      <legend class="form__legend">کاربران را اضافه کنید</legend>
       <div class="form__container">
-         <input id="chat-users" type="text" name="chatUsers" class="input form__input form__input_auth" placeholder="Введите логин пользователя" accept="image/*" required>
+         <input id="chat-users" type="text" name="chatUsers" class="input form__input form__input_auth" placeholder="نام کاربری را وارد کنید" accept="image/*" required>
          <p class="error error_type_chat-users" id="chatUsers"></p>
       </div>
       <div class="form__users-container"></div>
