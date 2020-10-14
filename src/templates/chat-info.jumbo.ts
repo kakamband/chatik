a04c734573d136fee70a58770e07cf5efaaa6f64
kakamband <70383870+kakamband@users.noreply.github.com>
2020-10-14 15:30:57 +0400

    Update chat-info.jumbo.ts

--- src/templates/chat-info.jumbo.ts
+++ src/templates/chat-info.jumbo.ts
@@ -2,5 +2,5 @@ export const chatInfo = `<div class="chat-info">
     <img class="chat-info__avatar" src="{{chatAvatar}}" alt="{{chatTitle}}" />
     <h1 class="chat-info__name">{{chatTitle}}</h1>
     <div class="chat-info__users"></div>
-    <button id="chat-info-button-back" class="chat-info__button" type="button">Назад к чатам</button>
+    <button id="chat-info-button-back" class="chat-info__button" type="button">بازگشت به چت ها</button>
 </div>`;
