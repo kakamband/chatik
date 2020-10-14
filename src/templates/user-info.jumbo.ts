469057d9ae9a907909e87d52439b4344a4f090a6
kakamband <70383870+kakamband@users.noreply.github.com>
2020-10-14 15:40:27 +0400

    Update user-info.jumbo.ts

--- src/templates/user-info.jumbo.ts
+++ src/templates/user-info.jumbo.ts
@@ -4,15 +4,15 @@ export const userInfo = `
    <fieldset class="form__fieldset" name="fieldset">
       <legend class="form__legend">{{first_name}}</legend>
       <div class="form__container">
-         <input type="text" name="first_name" class="input form__input form__input_user" id="user-first-name" placeholder="Введите имя" pattern="^([\\w\\W]{8,30})$" value="{{first_name}}" required>
+         <input type="text" name="first_name" class="input form__input form__input_user" id="user-first-name" placeholder="اسمت رو وارد کن" pattern="^([\\w\\W]{8,30})$" value="{{first_name}}" required>
          <p class="error error_type_first-name" id="first_name"></p>
       </div>
       <div class="form__container">
-         <input type="text" name="second_name" class="input form__input form__input_user" id="user-second-name" placeholder="Введите фамилию" pattern="^([\\w\\W]{8,30})$" value="{{second_name}}" required>
+         <input type="text" name="second_name" class="input form__input form__input_user" id="user-second-name" placeholder="فامیلیت رو وارد کن" pattern="^([\\w\\W]{8,30})$" value="{{second_name}}" required>
          <p class="error error_type_second-name" id="second_name"></p>
       </div>
       <div class="form__container">
-         <input type="text" name="display_name" class="input form__input form__input_user" id="user-display-name" placeholder="Введите отображаемое имя" pattern="^([\\w\\W]{8,30})$" value="{{display_name}}" required>
+         <input type="text" name="display_name" class="input form__input form__input_user" id="user-display-name" placeholder="نام نمایشیت رو وارد کنید" pattern="^([\\w\\W]{8,30})$" value="{{display_name}}" required>
          <p class="error error_type_display-name" id="display_name"></p>
       </div>
       <div class="form__container">
@@ -24,7 +24,7 @@ export const userInfo = `
          <p class="error error_type_email" id="login"></p>
       </div>
       <div class="form__container">
-         <input type="tel" name="phone" class="input form__input form__input_user" id="user-phone" placeholder="Введите телефон" pattern="^(\\+7((\\(\\d{3}\\)\\s?\\d{3}-\\d{2}-\\d{2})|(\\s\\d{3}-\\d{3}-\\d{2}-\\d{2})|(\\d{10})))|(8\\d{10})$" value="{{phone}}" required>
+         <input type="tel" name="phone" class="input form__input form__input_user" id="user-phone" placeholder="شماره همراهت رو وارد کنید" pattern="^(\\+7((\\(\\d{3}\\)\\s?\\d{3}-\\d{2}-\\d{2})|(\\s\\d{3}-\\d{3}-\\d{2}-\\d{2})|(\\d{10})))|(8\\d{10})$" value="{{phone}}" required>
          <p class="error error_type_email" id="phone"></p>
       </div>
       <div class="form__container"><button type="submit" class="common form__button" disabled>
