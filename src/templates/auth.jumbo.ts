0ee7228a87a9a8f3d279f7da4d487d507bc52c00
kakamband <70383870+kakamband@users.noreply.github.com>
2020-10-14 15:27:20 +0400

    Update auth.jumbo.ts

--- src/templates/auth.jumbo.ts
+++ src/templates/auth.jumbo.ts
@@ -1,7 +1,7 @@
 export const auth = `
 <form class="form form_auth" name="auth" id="auth" novalidate="">
    <fieldset class="form__fieldset" name="fieldset">
-      <legend class="form__legend">Авторизация</legend>
+      <legend class="form__legend">مجوز</legend>
       <div class="form__container">
          <input type="text" name="login" class="input form__input form__input_auth" id="authorization-email" placeholder="{{loginPlaceholder}}" pattern="{{loginPattern}}" required>
          <p class="error error_type_login" id="login"></p>
